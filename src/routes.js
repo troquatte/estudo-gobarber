42d47781c7e1b3a037721c34f169ce4b65e41882
Dener Troquatte <denertroquatte@MacBook-Pro-de-Dener.local>
2020-02-01 01:30:04 -0300

    Criação arquitetura no NodeJs

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/users';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-	return res.json({ message: 'Babilonia 2323' });
+routes.get('/', async (req, res) => {
+	const user = await User.create({
+		name: 'Dener Troquatte2',
+		email: 'd.troquatte201@yahoo.com.br',
+		password_hash: '123456'
+	});
+	return res.json(user);
 });
 
 export default routes;
